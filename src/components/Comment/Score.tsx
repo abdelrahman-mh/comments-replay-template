f5e307949f97b8000d9702bcfb2d18a2a6fc6d0a
abdelrahman-mh <abdelrahman.mh3@gmail.com>
2024-03-27 01:10:07 +0200

    hot-fix

--- src/components/Comment/Score.tsx
+++ src/components/Comment/Score.tsx
@@ -17,7 +17,7 @@ const Score: React.FC<Props> = ({ score, parentId, commentId }) => {
       <span className='comment__score-button increase f-c' onClick={handleIncreaseScore}>
         <img alt='icon-plus' src='./images/icon-plus.svg' />
       </span>
-      <span className='comment__score-value'>{score}</span>
+      <span className='comment__score-value f-c'>{score}</span>
       <span className='comment__score-button decrease f-c' onClick={handleDecreaseScore}>
         <img alt='icon-minus' src='./images/icon-minus.svg' />
       </span>
