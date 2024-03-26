fb3994ebac8396d83396c892decc0b027fee91be
abdelrahman-mh <abdelrahman.mh3@gmail.com>
2024-03-26 22:53:13 +0200

    enable GitHub pages in vite file, some enhancment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['./src/**/*'],
+  },
+  base: 'https://abdelrahman-mh.github.io/age-calculator/',
+  css: {
+    preprocessorOptions: {
+      sass: {
+        outputStyle: 'compressed',
+        sourceMap: true,
+        includePaths: ['src/styles'],
+        precision: 6,
+      },
+    },
+  },
+});
